13232fdb9d754c1ecb5589705ff5f059b44ba888
hirwavestine <gilhirwa22@gmail.com>
2019-04-23 21:44:06 +0200

    setting up the Login route

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const bcrypt = require("bcryptjs");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://127.0.0.1/LOGIN", { useNewUrlParser: true }, () => {
   console.log("connected");
 });
 //create a register route
-app.post("/register", (req, res) => {
+app.post("/regist er", (req, res) => {
   const newUser = new User();
   newUser.email = req.body.email;
   newUser.password = req.body.password;
@@ -31,7 +33,21 @@ app.post("/register", (req, res) => {
     });
   });
 });
-//save newUser
+//login user
+app.post("/login", (req, res) => {
+  user.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      bcrypt.compare(req.body.password, user.password, (err, matched) => {
+        if (err) return err;
+        if (matched) {
+          res.send("USER WAS ABLE TO LOGIN");
+        } else {
+          res.send("USER NOT ABLE TO LOGIN");
+        }
+      });
+    }
+  });
+});
 
 app.listen(4111, () => {
   console.log("listening on port 4111");
